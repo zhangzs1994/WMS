0b9194877bbbeca0b0d75dff51290dfcc3ca88bd
zhangzs1994 <1392842928@qq.com>
2017-06-27 09:54:38 +0800

    second commit

--- app/src/main/java/com/ycsx/www/wms/common/API.java
+++ app/src/main/java/com/ycsx/www/wms/common/API.java
@@ -5,7 +5,7 @@ package com.ycsx.www.wms.common;
  */
 public class API {
     public static final String URL="http://192.168.0.88:8080/YC_Service/";     //服务器地址
-    public static final String URL1="http://192.168.0.20:8080/JobHunting/"; //测试地址
+    public static final String URL1="http://192.168.0.19:8080/JobHunting/"; //测试地址
     public static final String URL2="http://192.168.0.13:8080/JobHunting/"; //测试地址
     public static String authorizationCode="YCWMS-20170522-XXX";  //验证码
     public static String downLoadUrl="";   //APP下载地址
